ae4aece9fbec0e3fed8a1d7ddec81a2dce651ece
Johnpaul470 <pogicruz213@gmail.com>
2019-01-26 13:39:43 +0800

    new example

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('hello');
+const express = require('express');
+const path = require('path');
+
+const server = express();
+
+server.use(express.static(path.join('build')));
+
+server.listen(5000);
+console.log('server running at port 5000');
